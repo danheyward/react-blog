c71e70e65a017a5d299e011ee4f7cdedcd3c7d2c
Daniel Heyward <danheyward@gmail.com>
2018-03-07 09:01:39 -0800

    blog pulse

--- src/Post.js
+++ src/Post.js
@@ -3,6 +3,27 @@ import Comment from './Comment';
 import Author from './Author';
 
 class Post extends Component {
+  constructor(props) {
+    super()
+    this.state = {
+      body: props.body,
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(e) {
+    this.setState({
+      body: e.target.value
+    })
+  }
+
+  changeBody(e) {
+    let newBody = prompt('What do you wanna say?')
+    this.setState({
+      body: newBody
+    })
+  }
+
   render() {
 
     const { title, authors, body, comments } = this.props
@@ -19,7 +40,9 @@ class Post extends Component {
       <div className='div-wrapper'>
         <h1 className='title'>{title}</h1>
         {allAuthors}
-        <p>{body}</p>
+        <p>{this.state.body}</p>
+        <input value={this.state.value} onChange={this.handleChange}/>
+        <button onClick={ (e) => this.changeBody(e) }>Change Body</button>
         <h3 className='comments-title'>Comments:</h3>
         {allComments}
       </div>
